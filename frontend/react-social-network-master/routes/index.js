1b8d5360c441f60bc9f28e9a2260cb1b1d1ecac3
Justin Park <justin.s.park77@gmail.com>
2019-09-15 10:47:46 -0400

    removed unnecessary files

--- frontend/react-social-network-master/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/api/test', function(req, res, next) {
-  res.json({success: true});
-});
-
-module.exports = router;
